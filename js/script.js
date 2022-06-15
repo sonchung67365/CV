259fb1b55e2be09d1fa1d3ce7c8b4e153735e851
sonchung67365 <sonchung67365@gmail.com>
2022-06-15 09:24:11 +0700

    update

--- js/script.js
+++ js/script.js
@@ -10,4 +10,21 @@ $(document).ready(function(){
         $('header').removeClass('toggle');
     });
 
+    // smooth scrolling
+
+    //$('a[href*="#"]').on('click', function(e){
+
+    //    e.preventDefault();
+
+    //    $('html, body').animate({
+
+    //        scrollTop : $($(this).attr('href')).offset().top,
+
+    //    },
+    //        500,
+    //        'linear'
+    //    );
+
+    //});
+
 })
